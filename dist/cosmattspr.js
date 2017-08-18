e116d4a2d520fa8bff2579328e8f279e68be1d59
Adnan Kamili <adnan.kamili@pearson.com>
2017-08-18 14:48:39 +0530

    updated js

--- dist/cosmattspr.js
+++ dist/cosmattspr.js
@@ -179,27 +179,6 @@ define('css!../css/cosmattspr',[],function(){});
 
     setTimeout(function () {
       Leonardo.scripts.add($leonardoPlugin[0], settings.config, settings.correctData);
-
-      // if (settings.correctData) {
-      //   self.append('<div class="btn-container mb-5"></div>');
-      //   var $btnContainer = self.find(".btn-container");
-      //   $btnContainer.append('<div class="float-left"><button type="button" id="reset-btn" class="btn mt-2 btn-primary btn-inverse">Reset</button></div>');
-      //   var $resetBtn = $btnContainer.find("#reset-btn");
-      //   $resetBtn.click(function () {
-      //     Leonardo.scripts.reset($leonardoPlugin[0]);
-      //   });
-      //   $btnContainer.css({
-      //     'width': $leonardoPlugin.find('.DLSLeonardo #grid').outerWidth(),
-      //     'margin': "0 auto"
-      //   });
-      //   $btnContainer.append('<div class="text-right"><button type="button" id="cosmatt-check-answer" class="btn mt-2 mr-3 btn-primary btn-inverse">Check My Work</button></div>');
-
-      //   var $checkAnsBtn = $btnContainer.find("#cosmatt-check-answer");
-      //   $checkAnsBtn.click(function () {
-      //     Leonardo.scripts.checkAnswer($leonardoPlugin[0], settings.correctData);
-      //   });
-      // }
-
       $leonardoPlugin.on('resize', function () {
         var $btnContainer = self.find(".btn-container");
         if ($btnContainer.length) {
@@ -223,7 +202,9 @@ define('css!../css/cosmattspr',[],function(){});
 
 
     var updateSheet = function (params) {
-      Leonardo.scripts.updateData($leonardoPlugin[0], JSON.parse(params.configData.value));
+      if(params && params.configData) {
+        Leonardo.scripts.updateData($leonardoPlugin[0], JSON.parse(params.configData.value));
+      }
     }
 
     var markAnswers = function (params) {
@@ -232,7 +213,7 @@ define('css!../css/cosmattspr',[],function(){});
 
     setInterval(function(){
       assessmentNotifier();
-    }, 3000);
+    }, 5000);
 
     return {
       ref: this,
