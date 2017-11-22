7ed7b741ebdc1e2561c5448b35df1fdc42fdb6fa
Adnan Kamili <adnan.kamili@pearson.com>
2017-11-22 17:14:51 +0530

    updated numberformatter

---
+++ src/libs/number-formatter/dist/NumberFormatter.d.ts
@@ -0,0 +1,8 @@
+import { INumberFormatterOptions } from "./INumberFormatterOptions";
+export declare class NumberFormatter {
+    private superscripts;
+    private options;
+    constructor(settings: INumberFormatterOptions);
+    format(originalInput: any, isEditable?: boolean): string;
+    toSuperscript(exponent: string): string;
+}
